b60ae8d9f8d04c74dcf9acf093a87a6424b76e93
Udaka-Hayerson <mikehayerson@gmail.com>
2021-07-25 17:21:41 +0300

    алгоритм выбора сложности - ультрасырой

---
+++ tamplate_123.py
@@ -0,0 +1,19 @@
+print(task_generator(int_time))
+print(task_generator_hard(int_time))
+
+simple = int(input('''Enter a number from 1 to 10 if you want a simpler problem
+or enter more than 10 if you want a more difficult problem  : ''').lower())
+# введите число от 1 до 10 если хотите задачку попроще или
+# введите больше 10 если хотите задачку посложнее
+yes_no = input('write \"yes\" if you want to continue  :').lower()
+
+if simple <= 10:
+    while yes_no == 'yes':
+        task_generator(int_time)
+        yes_no = input('write \"yes\" if you want to continue : ').lower()
+elif simple > 10:
+    while yes_no == 'yes':
+        task_generator_hard(int_time)
+        yes_no = input('write \"yes\" if you want to continue : ').lower()
+
+print(' счастливый конец ')

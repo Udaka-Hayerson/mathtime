6c6bfc5c40673c7baf2fa9955d6a2fd565b216ba
Udaka-Hayerson <mikehayerson@gmail.com>
2021-07-29 01:31:56 +0300

    приветствие Бота

---
+++ Decorators.py
@@ -0,0 +1,44 @@
+## HOF
+
+# HOF high order function (lo function!')
+#
+# myDecoratedNewFunction = myHOF(sayHello)
+#
+# myDecoratedNewFunction()
+# myDecoratedNewFunction()
+# myDecoratedNewFunction()
+#
+#
+# функция высшего порядка)
+# def myHOF(anyFunction):
+#     print(f'function has benn runned at {datetime.now().time()}')
+#
+#     return anyFunction
+#     # myDecoratedNewFunction == anyFunction
+#
+# def sayHello():
+#     print('Hello from sayHel
+
+
+### DECORATORS
+
+# HOF high order function (функция высшего порядка)
+def myDecorator(anyFunction):
+    def newFunction(user):
+        print(f'function has benn runned by {user} at {datetime.now().time()}')
+        anyFunction()
+
+    return newFunction
+
+@myDecorator
+def sayHello():
+    print('Hello from sayHello function!')
+
+
+
+
+# myDecoratedNewFunction = myDecorator(sayHello)
+#
+# myDecoratedNewFunction('Loik')
+# myDecoratedNewFunction('Vasya')
+# myDecoratedNewFunction('Natasha')
--- bot.py
+++ bot.py
@@ -1,23 +1,22 @@
 import telebot
-from prettyprinter import pprint
+
 from mathuynya import task_generator, task_generator_hard, int_time
 from judge import verno_neverno
+
 TOKEN = '1773331772:AAH-JkEcVkl1mqcgXdE5GdiTMrE01C-hhio'
 bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    bot.send_message(message.chat.id, 'Приветствую Юдзьверь. Я Матбот, бот для любителей себя помучать математикой', parse_mode='html')
+
+@bot.message_handler(commands=['help'])
+def welcome(message):
+    bot.send_message(message.chat.id, 'Я ничем не могу тебе помочь , можешь поныть как тебе тяжело жить но получишь '
+                                      'ты только новую задачку , и проблем станет на одну больше', parse_mode='html')
 
 @bot.message_handler(content_types=['text'])
 def lalala(message):
     result = task_generator(int_time)
     #result = 'little big'
     bot.send_message(message.chat.id, result)
-@bot.message_handler()
-def gogogo(message):
-
-verno_neverno(answer, int_time)
-
-    #pprint(message)
-
-
-# import pprint
-# pp = pprint.PrettyPrinter(indent=4)
-# pp.pprint(mydict)
